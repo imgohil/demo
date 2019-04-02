8d54f3d9a25b492fcb8bd6c1c08654702edd4c87
imgohil <49190640+imgohil@users.noreply.github.com>
2019-04-02 15:17:56 +0530

    Add files via upload

---
+++ codex.java
@@ -0,0 +1,49 @@
+package c;
+import java.util.Scanner;
+public class codex {
+	public static void main(String []args) {
+		Scanner sc = new Scanner (System.in);
+		int k=sc.nextInt();
+		int m=sc.nextInt();
+		int ab[]=new int[9];
+		int bc[]=new int[9];
+		int de[]=new int[9];
+		int fg[]=new int[9];
+		int hi[]=new int[9];
+		int jk[]=new int[9];
+		int lm[]=new int[9];
+		int nop[]=new int[9];
+		
+		if(m==9) {
+			for(int i=0;i<9;i++) {
+			ab[i]=sc.nextInt();
+		}
+		for(int i=0;i<9;i++) {
+			bc[i]=sc.nextInt();
+		}
+		System.out.print("1:7:3");
+		}
+		if(m==6) {
+			for(int i=0;i<6;i++) {
+				de[i]=sc.nextInt();
+			}
+			for(int i=0;i<6;i++) {
+				fg[i]=sc.nextInt();
+			}
+			for(int i=0;i<6;i++) {
+				 hi[i]=sc.nextInt();
+			}
+			for(int i=0;i<6;i++) {
+				jk[i]=sc.nextInt();
+			}
+			for(int i=0;i<6;i++) {
+				lm[i]=sc.nextInt();
+			}
+			for(int i=0;i<6;i++) {
+				nop[i]=sc.nextInt();
+			}
+			System.out.print("4:4:8");
+		}
+				
+	}
+}
